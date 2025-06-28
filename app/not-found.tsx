080d345d4c92f700ba26329f56b328cf3c181343
Ahza r. <ahzathenation@gmail.com>
2025-06-28 12:21:01 +0700

    ah

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -2,13 +2,14 @@ import Title from '@/components/Layout/Title'
 import Footer from '@/components/UI/Footer'
 import Navbar from '@/components/UI/Navbar'
 import Link from 'next/link'
-import { locales } from '@/libs/i18n'
-import { translations } from '@/libs/i18n/translations'
- 
+import { getTranslations } from '@/libs/i18n'
+
 export default function NotFound() {
+  const locale = 'en';
+  const translations = getTranslations(locale);
   return (
     <div className='h-screen flex flex-col justify-between'>
-      <Navbar locale={locales[0]} translations={translations}/>
+      <Navbar locale={locale} translations={translations}/>
       <div className="px-4 sm:mx-auto max-w-3xl my-20 flex flex-col items-center justify-center">
         <Title emoji="🤔">404</Title>
         <h2 className='mb-2 text-center'>Oops! There is no such page here...</h2>
@@ -19,7 +20,7 @@ export default function NotFound() {
           Back to Home
         </Link>
       </div>
-      <Footer  locale={locales[0]} translations={translations}/>
+      <Footer  locale={locale} translations={translations}/>
     </div>
   )
 }
