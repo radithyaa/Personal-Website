2b14f052f3fadcf100dfbb27ca1e772b307ed25a
ahja <136820094+radithyaa@users.noreply.github.com>
2025-06-28 07:32:50 +0700

    Fix hydration errors by removing nested HTML tags

--- app/[locale]/layout.tsx
+++ app/[locale]/layout.tsx
@@ -1,11 +1,6 @@
 import type { Metadata } from "next";
-import { Grotesk } from "@/libs/font";
-import { Analytics } from "@vercel/analytics/react";
-import { SpeedInsights } from "@vercel/speed-insights/next";
 import { isValidLocale, type Locale } from "@/libs/i18n";
 import { notFound } from "next/navigation";
-
-import "../globals.css";
 
 export const metadata: Metadata = {
   metadataBase: new URL("https://ahzaradithya.site"),
@@ -44,38 +39,13 @@ export function generateStaticParams() {
   ]
 }
 
-export default function RootLayout({
+export default async function LocaleLayout({
   children,
   params,
 }: Readonly<{
   children: React.ReactNode;
   params: Promise<{ locale: string }>;
 }>) {
-  return (
-    <html lang="en">
-      <body className={`${Grotesk.className} antialiased ` }>
-        <LocaleValidator params={params}>
-          {children}
-        </LocaleValidator>
-        <Analytics />
-        <SpeedInsights />
-        <script
-          defer
-          src="https://cloud.umami.is/script.js"
-          data-website-id="e9a956c2-369e-471d-a48e-d2e94e587bff"
-        ></script>
-      </body>
-    </html>
-  );
-}
-
-async function LocaleValidator({ 
-  children, 
-  params 
-}: { 
-  children: React.ReactNode;
-  params: Promise<{ locale: string }>;
-}) {
   const { locale } = await params;
   
   if (!isValidLocale(locale)) {
