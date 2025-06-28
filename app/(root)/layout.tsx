a84efac6066bfc000047b1f79c7784684b9f7c8d
Ahza r. <ahzathenation@gmail.com>
2025-06-28 09:06:31 +0700

    fix bug2

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,13 +1,15 @@
 import Footer from "@/components/UI/Footer";
 import Navbar from "@/components/UI/Navbar";
 import React, { ReactNode } from "react";
+import { locales } from "@/libs/i18n";
+import { translations } from "@/libs/i18n/translations";
 
 export default function layout({ children }: { children?: ReactNode }) {
   return (
     <>
-      <Navbar />
+      <Navbar locale={locales[0]} translations={translations}/>
       <main className="px-4 sm:mx-auto min-h-[57.5vh] max-w-3xl my-20">{children}</main>
-      <Footer />
+      <Footer translations={translations} />
     </>
   );
 }
